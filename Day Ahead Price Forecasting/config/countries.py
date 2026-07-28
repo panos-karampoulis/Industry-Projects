51b76237f3d8832d63414d003f112589016b7856
panos-karampoulis <panoskrp7@gmail.com>
2026-07-28 14:43:57 +0300

    Add files via upload

---
+++ Day Ahead Price Forecasting/config/countries.py
@@ -0,0 +1,92 @@
+COUNTRIES = {
+
+    "Germany": {
+
+        "code": "DE_LU",
+
+        "domain": "10Y1001A1001A82H",
+
+        "lat": 52.5200,
+
+        "lon": 13.4050,
+
+        "timezone": "Europe/Berlin"
+
+    },
+
+
+    "France": {
+
+        "code": "FR",
+
+        "domain": "10YFR-RTE------C",
+
+        "lat": 48.8566,
+
+        "lon": 2.3522,
+
+        "timezone": "Europe/Paris"
+
+    },
+
+
+    "Italy": {
+
+        "code": "IT",
+
+        "domain": "10YIT-GRTN-----B",
+
+        "lat": 41.9028,
+
+        "lon": 12.4964,
+
+        "timezone": "Europe/Rome"
+
+    },
+
+
+    "Netherlands": {
+
+        "code": "NL",
+
+        "domain": "10YNL----------L",
+
+        "lat": 52.3676,
+
+        "lon": 4.9041,
+
+        "timezone": "Europe/Amsterdam"
+
+    },
+
+
+    "Greece": {
+
+        "code": "GR",
+
+        "domain": "10YGR-HTSO-----Y",
+
+        "lat": 37.9838,
+
+        "lon": 23.7275,
+
+        "timezone": "Europe/Athens"
+
+    },
+
+
+    "Austria": {
+
+        "code": "AT",
+
+        "domain": "10YAT-APG------L",
+
+        "lat": 48.2082,
+
+        "lon": 16.3738,
+
+        "timezone": "Europe/Vienna"
+
+    }
+
+}
